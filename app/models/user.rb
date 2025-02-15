681561f45dbdcc5be9d81a540ee1c7a57b3bb04c
yosizumi1919 <kplmg2030@gmail.com>
2025-02-15 23:13:22 +0900

    ユーザー管理機能①

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,22 @@ class User < ApplicationRecord
          validates :kana_first_name, presence: true
          validates :kana_last_name, presence: true
          validates :birth_date, presence: true
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX
+
+KANJI_FIRST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+validates_format_of :kanji_first_name, with:KANJI_FIRST_NAME_REGEX
+
+KANJI_LAST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+validates_format_of :kanji_last_name, with:KANJI_LAST_NAME_REGEX
+
+KANA_FIRST_NAME_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+validates_format_of :kana_first_name, with:KANA_FIRST_NAME_REGEX
+
+KANA_LAST_NAME_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+validates_format_of :kana_last_name, with:KANA_LAST_NAME_REGEX
+
 end
+
+
