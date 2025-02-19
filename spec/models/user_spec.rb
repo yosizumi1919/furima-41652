934f07ab683c680d7337c88fd71b37c262b578f8
yosizumi1919 <kplmg2030@gmail.com>
2025-02-19 19:55:00 +0900

    修正②

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
+      it 'passwordに全角文字が入っていると登録できない' do
+        @user.password = '1111aaa山田'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
